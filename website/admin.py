5dc710ed0b9f8d24e53f1462ef4a4e93d11b6723
Shashi Kumar <shashiessp@gmail.com>
2015-10-20 11:26:22 +0530

    Add date joined to the user admin list

--- website/admin.py
+++ website/admin.py
@@ -37,4 +37,10 @@ admin.site.register(Bounty)
 #     list_display = ('user', 'rank', 'price', 'date'),
 # )
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
