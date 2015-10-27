2d4690e96291e852ac3477bb2d1aa596f7f180c6
pacufist <pacufist@gmail.com>
2015-10-27 18:41:06 +0000

    Some fixes conciedering Issue #1

---
+++ website/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Issue,Bounty
+
+class IssueCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Issue
+        fields = ('title','language','number')
+
+class BountyCreateForm(forms.ModelForm):
+
+    class Meta:
+        model = Bounty
+        fields = ('price',)
