1f84f300dfa5b3e61b17d3ea12412ed39246de76
Sean <sean@alphaonelabs.com>
2015-10-28 21:20:10 -0400

    add username and id to admin list

--- website/admin.py
+++ website/admin.py
@@ -40,7 +40,7 @@ admin.site.register(XP)
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
+UserAdmin.list_display = ('id','username','email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
