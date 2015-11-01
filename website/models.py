421784bb2b96393ec71200063bab322c3fa312d6
Sean <sean@alphaonelabs.com>
2015-11-01 02:23:57 -0500

    fix gravatars

--- website/models.py
+++ website/models.py
@@ -155,7 +155,7 @@ class Bounty(models.Model):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name="profile")
+    user = models.OneToOneField(User, related_name="userprofile")
     balance = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     payment_service = models.CharField(max_length=255, null=True, blank=True)
     payment_service_email = models.CharField(max_length=255, null=True, blank=True, default='')
