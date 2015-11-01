a24b652be401434643c67637d92901602de0ff1e
Sean <sean@alphaonelabs.com>
2015-11-01 01:44:20 -0400

    adding more to admin lists, fixed typo on email, adding initial data for services.

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from website.models import Issue, Watcher, Service, UserProfile, Bounty, UserService, XP, Delta
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 class ServiceAdmin(admin.ModelAdmin):
      list_display=[] 
@@ -11,17 +13,25 @@ class BountyAdmin(admin.ModelAdmin):
     for x in Bounty._meta.get_all_field_names(): 
         list_display.append(str(x))
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display=[] 
+    for x in Bounty._meta.get_all_field_names(): 
+        list_display.append(str(x))
+
 class WatcherAdmin(admin.ModelAdmin):
     list_display=[] 
     for x in Watcher._meta.get_all_field_names(): 
         list_display.append(str(x))
         
-class  XPAdmin(admin.ModelAdmin):
+class XPAdmin(admin.ModelAdmin):
     list_display=[] 
     for x in XP._meta.get_all_field_names(): 
         list_display.append(str(x))
 
 class IssueAdmin(admin.ModelAdmin):
+    list_display=[] 
+    for x in Watcher._meta.get_all_field_names(): 
+        list_display.append(str(x))
     readonly_fields = ("created","modified")
     
 admin.site.register(Issue, IssueAdmin)
@@ -29,16 +39,15 @@ admin.site.register(UserService)
 
 admin.site.register(Service, ServiceAdmin)
 #admin.site.register(Watcher, WatcherAdmin)
-admin.site.register(UserProfile)
-admin.site.register(Bounty)
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Bounty, BountyAdmin)
 admin.site.register(XP)
 
 # admin.site.register(Delta,
 #     list_display = ('user', 'rank', 'price', 'date'),
 # )
 
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+
 
 UserAdmin.list_display = ('id','username','email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff')
 
