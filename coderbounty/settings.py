0b6f9f63773823a100c3d92dfc3de33fb0cc404d
Sean <sean@alphaonelabs.com>
2015-11-01 05:04:42 -0500

    add context processor

--- coderbounty/settings.py
+++ coderbounty/settings.py
@@ -70,6 +70,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'allauth.account.context_processors.account',
     'allauth.socialaccount.context_processors.socialaccount',
     'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.static', 
 )
 
 ROOT_URLCONF = 'coderbounty.urls'
