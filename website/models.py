af57782cb7349a03a8d7157b4280b1279dd720e7
Sean <sean@alphaonelabs.com>
2015-11-01 01:06:08 -0500

    removing unused code

--- website/models.py
+++ website/models.py
@@ -56,6 +56,7 @@ class Issue(models.Model):
         ('JavaScript', 'JavaScript'),
         ('Perl', 'Perl'),
         ('Ruby', 'Ruby'),
+        ('HTML', 'HTML'),
     )
 
     service = models.ForeignKey(Service, related_name='+')
@@ -89,9 +90,6 @@ class Issue(models.Model):
         if self.status == self.OPEN_STATUS:
             return timeuntil(self.bounties().aggregate(Min('ends'))['ends__min'], datetime.datetime.now()).split(',')[0]
         return timeuntil(self.modified + datetime.timedelta(days=3), datetime.datetime.now()).split(',')[0]
-
-    def watchers(self):
-        return Watcher.objects.filter(issue=self)
 
     def html_url(self):
         service = self.service
@@ -154,45 +152,6 @@ class Bounty(models.Model):
                 action.send(self.user, verb='updated bounty price', target=self)
 
         super(Bounty, self).save(*args, **kwargs)
-    
-
-
-class Watcher(models.Model):
-    user = models.ForeignKey(User)
-    issue = models.ForeignKey(Issue)
-    bounty = models.BooleanField(default=None)
-    status = models.BooleanField(default=None)
-
-
-class UserService(models.Model):
-    user = models.ForeignKey(User)
-    service = models.ForeignKey(Service, related_name='+')
-    username = models.CharField(max_length=255, null=True, blank=True)
-
-    def __unicode__(self):
-        return self.username
-
-    class Meta:
-        unique_together = ("user", "service")
-
-
-class XP(models.Model):
-    user = models.ForeignKey(User)
-    points = models.IntegerField()
-    what_for = models.CharField(max_length=255)
-    type = models.CharField(max_length=255, choices=(('coder', 'coder'), ('owner', 'owner')), null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-#class Badge(models.Model):
-#    icon = models.ImageField(upload_to='static/images/badges', blank=True)
-#    name = models.CharField(max_length=255)
-#    message = models.CharField(max_length=255)
-#    how_to = models.CharField(max_length=255)
-
-#class UserBadge(models.Model):
-#    user = models.ForeignKey(User)
-#    badge = models.ForeignKey(Badge)
-#    created = models.DateTimeField(auto_now_add=True)
 
 
 class UserProfile(models.Model):
@@ -220,26 +179,11 @@ class UserProfile(models.Model):
     def gravatar_winner(self, size=23):
         return self.gravatar(size=23)
 
-    def github_username(self):
-        service = Service.objects.get(name="Github")
-        return UserService.objects.get(user=self.user, service=service)
-
-    def google_code_username(self):
-        service = Service.objects.get(name="Google Code")
-        return UserService.objects.get(user=self.user, service=service)
-
-    def bitbucket_username(self):
-        service = Service.objects.get(name="Bitbucket")
-        return UserService.objects.get(user=self.user, service=service)
-
-
     def save(self, *args, **kwargs):
         #if new user signs up add it to the activity feed
         if self.pk is None:
             action.send(self.user, verb='signed up')
         super(UserProfile, self).save(*args, **kwargs)
-
-
 
     def __unicode__(self):
         return self.user.email
@@ -256,63 +200,12 @@ def create_profile(sender, **kwargs):
 post_save.connect(create_profile, sender=User)
 
 
-class DeltaManager(models.Manager):
-    def get_random_user(self):
-        ''' generate a random user to send out the delta '''
-        rank_min = self.get_query_set().aggregate(Min('rank'))['rank__min']
-        delta = random.choice(self.get_query_set().filter(rank=rank_min))
-        delta.rank += 1
-        delta.price = 5
-        delta.date = datetime.datetime.now()
-        delta.save()
-        return delta.user
-
-    def cleanup(self):
-        ''' clean up the delta not used over 24hours '''
-        day_before = datetime.datetime.now() - datetime.timedelta(seconds=86400 - 5)
-        for delta in self.get_query_set().filter(price__gt=0).filter(date__lt=day_before):
-            delta.price = 0
-            delta.save()
-
-
-class Delta(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    rank = models.IntegerField(default=0)
-    date = models.DateTimeField(default=datetime.datetime.now)
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    objects = DeltaManager()
-
-
-def on_new_user(instance, created, **kwargs):
-    if created:
-        Delta.objects.get_or_create(user=instance)
-
-
 TWITTER_MAXLENGTH = getattr(settings, 'TWITTER_MAXLENGTH', 140)
 
 
 def post_to_twitter(sender, instance, *args, **kwargs):
     """
     Post new saved objects to Twitter.
-
-    Example:
-        from django.db import models
-
-        class MyModel(models.Model):
-            text = models.CharField(max_length=255)
-            link = models.CharField(max_length=255)
-
-            def __unicode__(self):
-                return u'%s' % self.text
-
-            def get_absolute_url(self):
-                return self.link
-
-            # the following method is optional
-            def get_twitter_message(self):
-                return u'my-custom-twitter-message: %s - %s' \
-                        % (self.text, self.link)
-
         models.signals.post_save.connect(post_to_twitter, sender=MyModel)
     """
 
@@ -373,36 +266,3 @@ signals.post_save.connect(alert_winner, sender=Issue)
 #todo: fix this so it doesn't throw an error
 #signals.post_save.connect(post_to_twitter, sender=Bounty)
 signals.post_delete.connect(delete_issue, sender=Bounty)
-
-
-# class CoinManager(models.Manager):
-#     def add_coin_for_user(self, user, request=None):
-#         try:
-#             cookie_coins = int(request.COOKIES.get('coins', 0))
-#         except:
-#             cookie_coins = 0
-
-#         day_of_year = int(datetime.datetime.strftime(datetime.datetime.now(), "%j"))
-#         obj, created = self.get_query_set().get_or_create(user=user)
-#         if obj.day_of_year != day_of_year:
-#             # update UserProfile set coins +1
-#             try:
-#                 user_profile = user.get_profile()
-#                 if user_profile.coins == 0 and cookie_coins > 0:
-#                     user_profile.coins = cookie_coins
-#                 user_profile.coins += 1
-#                 user_profile.save()
-#             except UserProfile.DoesNotExist:
-#                 return False
-#             else:
-#                 # save the newest day of year
-#                 obj.day_of_year = day_of_year
-#                 obj.save()
-#                 return True
-#         return False
-
-
-# class Coin(models.Model):
-#     user = models.OneToOneField(User)
-#     day_of_year = models.PositiveIntegerField(default=0)
-#     #objects = CoinManager()
