39d8911dcd8d1b057e5f025bd2b56563be83cd35
Sean <sean@alphaonelabs.com>
2015-11-01 22:51:06 -0500

    support for commas

--- website/templatetags/total.py
+++ website/templatetags/total.py
@@ -7,4 +7,4 @@ register = template.Library()
 
 @register.simple_tag
 def bounty_total():
-    return int(UserProfile.objects.aggregate(Sum('balance'))['balance__sum'])
+    return "{:,.0f}".format(UserProfile.objects.aggregate(Sum('balance'))['balance__sum'])
