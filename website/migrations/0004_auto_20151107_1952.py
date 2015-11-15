c105c8e936aef1a9160e01301b3c0d871b9183e7
Sean <sean@alphaonelabs.com>
2015-11-14 23:53:07 -0500

    change migration to work on production

--- website/migrations/0004_auto_20151107_1952.py
+++ website/migrations/0004_auto_20151107_1952.py
@@ -15,43 +15,7 @@ class Migration(migrations.Migration):
         migrations.RemoveField(
             model_name='delta',
             name='user',
-        ),
-        migrations.DeleteModel(
-            name='Delta',
-        ),
-        migrations.AlterUniqueTogether(
-            name='userservice',
-            unique_together=None,
-        ),
-        migrations.RemoveField(
-            model_name='userservice',
-            name='service',
-        ),
-        migrations.RemoveField(
-            model_name='userservice',
-            name='user',
-        ),
-        migrations.DeleteModel(
-            name='UserService',
-        ),
-        migrations.RemoveField(
-            model_name='watcher',
-            name='issue',
-        ),
-        migrations.RemoveField(
-            model_name='watcher',
-            name='user',
-        ),
-        migrations.DeleteModel(
-            name='Watcher',
-        ),
-        migrations.RemoveField(
-            model_name='xp',
-            name='user',
-        ),
-        migrations.DeleteModel(
-            name='XP',
-        ),
+        ),    
         migrations.AlterModelOptions(
             name='issue',
             options={'ordering': ['-created']},
