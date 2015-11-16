ffafce7ab4a52f17d7be330309b16b21ddec4e74
Pedro Mourelle <pedromagnus@gmail.com>
2015-11-16 03:40:01 -0300

    When nothing to sum returned None and break.

--- website/templatetags/total.py
+++ website/templatetags/total.py
@@ -1,10 +1,13 @@
-import datetime
 from django import template
 from website.models import UserProfile
 from django.db.models import Sum
 
 register = template.Library()
 
+
 @register.simple_tag
 def bounty_total():
-    return "{:,.0f}".format(UserProfile.objects.aggregate(Sum('balance'))['balance__sum'])
+    _total = UserProfile.objects.aggregate(Sum('balance'))['balance__sum']
+    if _total is None:
+        return "0"
+    return "{:,.0f}".format(_total)
