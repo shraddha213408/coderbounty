0c6339a99e38774939a321bd529aaae779616f75
Sean <sean@alphaonelabs.com>
2015-11-21 13:09:53 -0500

    fix?

--- website/views.py
+++ website/views.py
@@ -338,7 +338,7 @@ class LeaderboardView(ListView):
     template_name="leaderboard.html"
     
     def get_queryset(self):
-        return User.objects.all().order_by('-last_login','-userprofile__balance')
+        return User.objects.all().order_by('-userprofile__balance','-last_login')
     
     def get_context_data(self, **kwargs):
         context = super(LeaderboardView, self).get_context_data(**kwargs)
