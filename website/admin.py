e6eaed957198260c5b7150c505e8d894acb58eb7
Sean Auriti <sean@alphaonelabs.com>
2017-01-30 01:03:59 -0500

    fixes error

--- website/admin.py
+++ website/admin.py
@@ -56,7 +56,7 @@ class LogEntryAdmin(admin.ModelAdmin):
         return actions
 
 
-class SessionAdmin(ModelAdmin):
+class SessionAdmin(admin.ModelAdmin):
     def _session_data(self, obj):
         return obj.get_decoded()
     list_display = ['session_key', '_session_data', 'expire_date']
